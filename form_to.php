c6c1de69e7159cf58661794fa3c792a3b316cf81
yuenaka <yuki.u.1224@gmail.com>
2017-12-30 08:16:53 +0000

    仕掛中

--- form_to.php
+++ form_to.php
@@ -10,7 +10,15 @@
   $text = str_replace(array('\r\n','\r','\n'), '\n', $_POST["novel"]);
   // 改行コード毎に分割し配列に格納
   $text = explode("\n", $text);
-  print_r($text);
+  
+  foreach($text as $line) {
+      if (strpos($line, '*') !== false) {
+          $line = str_replace('*', '', $line);
+          // 変換文字位置
+          // ひらがな->カタカナ
+      }
+      echo nl2br($line);
+  }
 ?>
 <br />
 <a href="index.php">TOP</a>
