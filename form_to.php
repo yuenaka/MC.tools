0e2cf0b6aeac078fdc9db3994e6a20c0ce6292a4
yuenaka <yuki.u.1224@gmail.com>
2017-12-30 13:28:39 +0000

    不具合修正、コピペしやすいようフォーム形式で出力させるよう修正

--- form_to.php
+++ form_to.php
@@ -5,33 +5,38 @@
   <title>変換完了</title>
 </head>
 <body>
-  <?php
+<form>
+<textarea name="novel" cols="100" rows="15">
+<?php
   // 改行コード(\r\n, \r, \n) を全て \n に統一
   $text = str_replace(array('\r\n','\r','\n'), '\n', $_POST["novel"]);
   // 改行コード毎に分割し配列に格納
   $text = explode("\n", $text);
   
   foreach($text as $line) {
+    $line = strip_tags($line);
     if (strpos($line, '*') !== false) {
       $line = str_replace('*', '', $line);
-      $line = strip_tags($line);
       // 変換対象 句読点前を検出したい
-      $pattern = "/([、。！？」])/u";
+      $pattern = "/([、。！？」\n\s])/u";
       $array = preg_split($pattern, $line, -1, PREG_SPLIT_DELIM_CAPTURE);
 
       $line = "";
       foreach ($array as $work) {
         if (!preg_match($pattern, $work)) {
-            // 文字列の最後を全角カタカナに変換
+          // 文字列の最後を全角カタカナに変換
           $tmp = mb_convert_kana(mb_substr($work, -1), "KVC");
           $work = substr_replace($work, $tmp, -(strlen($tmp)));
         }
         $line .= $work;
       }
-      echo nl2br(print_r($line,true));
     }
-    ?>
-    <br />
-    <a href="index.php">TOP</a>
-  </body>
-  </html>
+    echo nl2br(print_r($line,true));
+  }
+  ?>
+</textarea>
+</form>
+  <br />
+  <a href="index.php">TOP</a>
+</body>
+</html>
